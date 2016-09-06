f574feecb7a5ad81ba98c9fe78ce4734d248faff
Ali Watters <ali.watters@gmail.com>
2016-09-05 19:22:39 -0700

    wip video-33

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
 import {Component} from 'angular2/core';
-import {CoursesComponent} from './courses.component';
-import {AuthorsComponent} from './authors.component';
+import {FavoriteComponent} from './favorite.component';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello World!</h1>
-                <courses></courses>
-                <authors></authors>
+    template: `
+                <favorite
+                [isFavorite]="post.isFavorite"
+                (change)="onFavoriteChange($event)">
+                </favorite>
               `,
-    directives: [CoursesComponent, AuthorsComponent]
+    directives: [FavoriteComponent]
 })
 
-export class AppComponent { }
+export class AppComponent {
+    post = {
+        title: "Title",
+        isFavorite: true
+    }
+
+    onFavoriteChange($event) {
+      console.log($event);
+    }
+}
--- app/favorite.component.ts
+++ app/favorite.component.ts
@@ -1,18 +1,21 @@
-import {Component} from "angular2/core";
+import {Component, Input, Output, EventEmitter} from "angular2/core";
 
 @Component({
-  selector: 'favorite',
-  template: `<i class="glyphicon"
+    selector: 'favorite',
+    template: `<i class="glyphicon"
               [class.glyphicon-star-empty]='!isFavorite'
               [class.glyphicon-star]='isFavorite'
               (click)="onClick()"
               ></i>`
 })
 
-export class Favorite {
-  isFavorite = false;
+export class FavoriteComponent {
+    @Input() isFavorite = false;
 
-  onClick(){
-    this.isFavorite = !this.isFavorite;
-  }
+    @Output() change = new EventEmitter();
+
+    onClick() {
+        this.isFavorite = !this.isFavorite;
+        this.change.emit({ newValue: this.isFavorite });
+    }
 }

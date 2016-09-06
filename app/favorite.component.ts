f80ccfd52544060ebed963b4acab0df35058ca95
Ali Watters <ali.watters@gmail.com>
2016-09-05 18:59:42 -0700

    wip video 28

---
+++ app/favorite.component.ts
@@ -0,0 +1,18 @@
+import {Component} from "angular2/core";
+
+@Component({
+  selector: 'favorite',
+  template: `<i class="glyphicon"
+              [class.glyphicon-star-empty]='!isFavorite'
+              [class.glyphicon-star]='isFavorite'
+              (click)="onClick()"
+              ></i>`
+})
+
+export class Favorite {
+  isFavorite = false;
+
+  onClick(){
+    this.isFavorite = !this.isFavorite;
+  }
+}

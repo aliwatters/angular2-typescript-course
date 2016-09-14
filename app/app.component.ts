633e3f894851b575f604b5075cecb11151a600e9
Ali Watters <ali.watters@gmail.com>
2016-09-14 08:51:14 -0500

    completed video-98

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,33 @@
-import {Component} from 'angular2/core';
-import {PasswordFormComponent} from './password-form.component';
+import {Component, OnInit} from 'angular2/core';
+import {PostService} from './post.service';
+import {HTTP_PROVIDERS} from 'angular2/http';
 
 
 @Component({
   selector: 'my-app',
   template: `
-  <password-form>
-  </password-form>
+    <i *ngIf="isLoading" class="fa fa-spinner fa-spin fa-3x"></i>
   `,
-  directives: [PasswordFormComponent]
+  providers: [PostService, HTTP_PROVIDERS]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLoading = true;
+
+  constructor(private _postService: PostService){
+    this._postService.createPost({
+      userId: 1,
+      title:'hello',
+      body: 'body'
+    });
+  }
+
+  ngOnInit() {
+    this._postService.getPosts()
+      .subscribe(posts => {
+        this.isLoading = false;
+        console.log(posts)
+      })
+  }
 
 }

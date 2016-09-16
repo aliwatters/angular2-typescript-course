1211bd7584222e55436a94dfde5537626a9a1a33
Ali Watters <ali.watters@gmail.com>
2016-09-15 21:20:13 -0500

    completed video 113

--- app/contact.component.ts
+++ app/contact.component.ts
@@ -1,10 +1,19 @@
 import {Component} from 'angular2/core';
+import {Router, CanDeactivate} from 'angular2/router';
 
 @Component({
-    templateUrl: '/app/contact.component.html'
+  templateUrl: '/app/contact.component.html'
 })
-export class ContactComponent {
-    onSubmit(form){
-        console.log(form);
-    }
+export class ContactComponent implements CanDeactivate {
+  constructor(private _router: Router) { }
+
+  onSubmit(form) {
+    console.log(form);
+    this._router.navigate(['Albums']);
+  }
+
+  routerCanDeactivate(next, previous) {
+    // if(this.form.dirty) { }
+    return confirm('are you sure?');
+  }
 }

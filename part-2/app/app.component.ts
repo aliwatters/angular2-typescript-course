7c132366b76bec3bb32516334a665da4a155fa98
Ali Watters <ali.watters@gmail.com>
2016-09-18 08:49:56 -0700

    completed video-122 - with code

---
+++ part-2/app/app.component.ts
@@ -0,0 +1,33 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {HomeComponent} from './home.component';
+import {NavBarComponent} from './navbar.component';
+import {PostsComponent} from './posts.component';
+import {UsersComponent} from './users.component';
+
+@RouteConfig([
+  {
+    path: '/', name: 'Home', component: HomeComponent, useAsDefault: true
+  },
+  { path: '/posts', name: 'Posts', component: PostsComponent },
+  { path: '/users', name: 'Users', component: UsersComponent },
+  { path: '/*other', name: 'Other', redirectTo: ['Home'] }
+])
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <navbar></navbar>
+  <div class="container">
+    <router-outlet></router-outlet>
+  </div>
+  `,
+  directives: [
+    NavBarComponent,
+    ROUTER_DIRECTIVES
+  ]
+})
+
+export class AppComponent {
+}

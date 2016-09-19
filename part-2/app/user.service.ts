ff6636935f6ea6385ab72814fe7e86b456e60f3c
Ali Watters <ali.watters@gmail.com>
2016-09-18 20:44:35 -0700

    completed video 124

---
+++ part-2/app/user.service.ts
@@ -0,0 +1,22 @@
+import {Http} from 'angular2/http';
+import {Injectable} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+
+@Injectable() export class UserService {
+  private _url = 'http://jsonplaceholder.typicode.com/users';
+
+  constructor(private _http: Http) {
+  }
+
+  getUsers() {
+    return this._http.get(this._url)
+      .map(res => res.json())
+  }
+
+  createUser(post) {
+    return this._http.post(this._url, JSON.stringify(post))
+      .map(res => res.json)
+  }
+}
--- part-2/app/users.component.ts
+++ part-2/app/users.component.ts
@@ -1,8 +1,28 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+
+import {UserService} from './user.service';
 
 @Component({
-  template: '<h1>Users</h1>'
+  templateUrl: 'app/users.component.html',
+  providers: [UserService]
 })
 
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  isLoading = true;
+  users = {};
+  userService;
+
+  constructor(private _userService: UserService) { }
+
+  ngOnInit() {
+    this._userService
+      .getUsers()
+      .subscribe(res => {
+        this.users = res
+      },
+      null,
+      () => {
+        this.isLoading = false;
+      });
+  }
 }

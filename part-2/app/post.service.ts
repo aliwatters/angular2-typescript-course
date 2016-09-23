d0f6387ca403031b4cd967f929b3032c3ea47929
Ali Watters <ali.watters@gmail.com>
2016-09-22 17:48:59 -0700

    completed video-139

---
+++ part-2/app/post.service.ts
@@ -0,0 +1,41 @@
+import {Http} from 'angular2/http';
+import {Injectable} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+
+@Injectable() export class PostService {
+  private _url = 'http://jsonplaceholder.typicode.com/posts';
+
+  constructor(private _http: Http) {
+  }
+
+  getPosts() {
+    return this._http.get(this._url)
+      .map(res => res.json())
+  }
+
+  getPost(id: number) {
+    return this._http.get(this.getPostUrl(id))
+      .map(res => res.json())
+  }
+
+  createPost(post) {
+    return this._http.post(this._url, JSON.stringify(post))
+      .map(res => res.json)
+  }
+
+  deletePost(id: number) {
+    return this._http.delete(this.getPostUrl(id))
+      .map(res => res.json)
+  }
+
+  updatePost(post) {
+    return this._http.put(this.getPostUrl(post.id), JSON.stringify(post))
+      .map(res => res.json)
+  }
+
+  private getPostUrl(id) {
+    return this._url + '/' + id;
+  }
+}
--- part-2/app/posts.component.ts
+++ part-2/app/posts.component.ts
@@ -1,8 +1,47 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {RouterLink} from 'angular2/router';
+
+import {PostService} from './post.service';
+import {SpinnerComponent} from './spinner.component';
 
 @Component({
-  template: '<h1>Posts</h1>'
+  templateUrl: '/app/posts.component.html',
+  providers: [PostService],
+  directives: [RouterLink, SpinnerComponent],
+  styles: [`
+        .posts li { cursor: default; }
+        .posts li:hover { background: #ecf0f1; }
+        .list-group-item.active,
+        .list-group-item.active:hover,
+        .list-group-item.active:focus {
+            background-color: #ecf0f1;
+            border-color: #ecf0f1;
+            color: #2c3e50;
+        }
+    `],
 })
 
 export class PostsComponent {
+  isLoading = true;
+  posts = [];
+  currentPost;
+
+  constructor(private _postService: PostService) { }
+
+  ngOnInit() {
+    this._postService
+      .getPosts()
+      .subscribe(res => {
+        this.posts = res
+      },
+      null,
+      () => {
+        this.isLoading = false;
+      });
+
+  }
+
+  displayPost(post) {
+    this.currentPost = post;
+  }
 }
